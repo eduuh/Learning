e3d2dc46a82d8168d5493d0b15ceccd487e67d15
eduuh <edwinkamaumuraya0@outlook.com>
2020-07-30 18:03:08 +0000

    Learning process! done for today.

--- framework/meanstack/express/Justexpress/myNewExpressSite/routes/index.js
+++ framework/meanstack/express/Justexpress/myNewExpressSite/routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  const date = new Date(1969, 6, 20);
+  //res.set('Date', date);
+  //res.set('Content-Type', 'text/html');
+  //res.set('Cache-Control', 'no-store');
+  //fresh and stale
+  //fresh returns true when it is not stale.
+  console.log(req.fresh);
   res.render('index', { title: 'Express' });
 });
 
