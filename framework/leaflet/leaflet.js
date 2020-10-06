3d3c08dc959dea8bdaa9076c11085e0110910390
eduuh <edwinkamaumuraya0@outlook.com>
2020-10-07 01:06:01 +0300

    spent time learning leafletjs

---
+++ framework/leaflet/leaflet.js
@@ -0,0 +1,92 @@
+// Online Maps
+// - "slipy maps" base maps
+// used in vast different ways, from visualization to anlytics
+// Some uses could be __Contact, Routing, searches, Events.
+// Online maps are a bunch of  images which are loaded at different zoom levels.
+// __Mapbox__ create maps out of vector layours and this is cool.
+//  - There is no concept of zoom levels.(you could adujust the map to any zoom function )
+
+// Leaflet. OpenSource.
+//  Google maps Api is a good library and it works all and it handle alot of markers.
+//  you need an api key.
+//    - Advantages of google maps. Can handle alot of marker data.
+//    - There are usage limits.
+//    - There is a big community for google maps.
+
+// Mapbox GL js
+//  - Based of leaflets but with vector concept.
+//  - Has a smooth transition, away from the zoom levels.
+//
+//
+//  Some questions to ask yourself?
+//  1. What kind of data do you have and need?
+//  2. What should your map look like.
+//
+// reference GeoJson Data: https://tools.ietf.org/html/rfc7946
+// LeafletJs
+//       - Best open source solution.
+//       - Use as npm, simple HTML
+//       - Plugins and good documentations.
+//   Leaflets takes in some options
+//  Leaflets support 3rd party plugins
+//
+
+// Data
+/* 
+- Data is curcial, often underestimated.
+- geoJSON, shapefiles , helpful tools.
+- Data is the first hard thing to get.
+Some Data Places you could find
+    - GEOJSON.io rewind-geojson mapshaper geojsonlint.
+  - Government 
+  - APIs. (Twitter. what are people say about a place)
+  - SpreadSheet
+  - Enterprice
+
+- Geospartial data does not change often (that the beautiy of this data.)
+
+- We are going to be using geojson data format mainly.
+  
+# useful sites.
+
+- https://eric.clst.org/tech/usgeojson/
+- https://mapshaperorg/
+   - use this site to view your geojson data
+- https://geojson.io
+   - Create geojson data out of openstreat map.
+   - map geojson.
+- mapster.me
+   - Check your geojson and fix it from here 
+- geojsonlint.com
+   - check the validity of geojsons.
+
+## leaflet Basic.
+- Shapes Basics.
+   -markers
+   - polygons
+   - circles
+   - base map choices.
+   - zooming and circles
+- Popups Icons.
+- GeoJsons
+- Bounds
+- Events
+- Design
+
+
+### Custom Marker options.
+
+- PopUps icons Dynamic , User Friendly
+- creating properly.
+- content from marker
+- custom styling
+
+1. You can bing popups on some shapes.
+
+## GeoJSONs
+
+- Efficiency     -Layers    - Styling   - PopupDisplay.
+   
+
+
+*/
