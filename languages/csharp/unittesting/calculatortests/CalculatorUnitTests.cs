5724296c68e45964d8bf6d82c96bc87ea83de69d
eduuh <edwinkamaumuraya0@outlook.com>
2020-11-20 14:01:49 +0300

    Finished reading on unit tests

---
+++ languages/csharp/unittesting/calculatortests/CalculatorUnitTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using calculatorLib;
+
+
+namespace calculatortests
+{
+    public class CalculatorUnitTests
+    {
+        [Fact]
+        public void TestAdding2and2()
+        {
+          // arrange
+          double a = 2;
+          double b = 2;
+          double expected = 4;
+          var calc = new Calculator();
+
+          //act 
+          double actual = calc.Add(a,b);
+          //assert
+          Assert.Equal(expected, actual);
+
+        }
+
+
+        [Fact]
+        public void TestAdding2and3()
+        {
+          // arrange
+          double a = 2;
+          double b = 3;
+          double expected = 5;
+          var calc = new Calculator();
+
+          //act 
+          double actual = calc.Add(a,b);
+          //assert
+          Assert.Equal(expected, actual);
+
+        }
+    }
+}
