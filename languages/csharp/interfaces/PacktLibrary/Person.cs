134fb76da1e2a87b84fe9723e7ae0923f2223fe6
eduuh <edwinkamaumuraya0@outlook.com>
2020-11-22 09:16:02 +0300

    Finished working with instance and Class methods

---
+++ languages/csharp/interfaces/PacktLibrary/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.shared
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> children = new List<Person>();
+
+        // method
+        public void WriteToConsole() {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+        // static method
+        public static Person Procreate(Person p1, Person p2) {
+
+            var baby = new Person {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.children.Add(baby);
+            p2.children.Add(baby);
+            return baby;
+        }
+
+        public static Person operator *(Person p1, Person p2){
+            return Person.Procreate(p1,p2);
+        }
+
+        // Instance Method to "multiply"
+        public Person ProcreateWtih(Person partner) {
+            return Procreate(this, partner);
+        }
+
+
+
+    }
+}
---
+++ languages/csharp/interfaces/PeopleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using  Packt.shared;
+
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var harry = new Person {Name = "Harry"} ;
+            var mary = new Person {Name = "mary"} ;
+            var jill = new Person {Name = "jill"} ;
+
+            // Call instance method
+
+            var baby1 = mary.ProcreateWtih(harry);
+
+            // call static method
+            var baby2 = Person.Procreate(harry, jill);
+            //using operator method
+            var baby = harry * jill;
+            WriteLine($"{harry.Name} has {harry.children.Count} children");
+            WriteLine($"{mary.Name} has {mary.children.Count} children");
+            WriteLine($"{jill.Name} has {jill.children.Count} children");
+
+           WriteLine();
+            Console.WriteLine("Procreate with * operator");
+
+            WriteLine(baby);
+
+            
+        }
+    }
+}
