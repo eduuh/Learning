d550af26b24c9049818a096f12e40bb68fd75da3
eduuh <edwinkamaumuraya0@outlook.com>
2020-11-29 23:58:25 +0300

    Worked with Udp protocal.

---
+++ languages/csharp/udp/console.udpclient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        for(int i=0;i<=40; i++){
+        IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+
+        byte[] sendbuf = Encoding.ASCII.GetBytes(i.ToString());
+        IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+        s.SendTo(sendbuf, ep);
+
+        Thread.Sleep(3000);
+        }
+
+        Console.WriteLine("Message sent to the broadcast address");
+    }
+}
---
+++ languages/csharp/udp/console.udpserver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class UDPListener
+{
+    private const int listenPort = 11000;
+
+    private  static void StartListener()
+    {
+        UdpClient listener = new UdpClient(listenPort);
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+        try
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for broadcast");
+                byte[] bytes = listener.Receive(ref groupEP);
+
+                Console.WriteLine($"Received broadcast from {groupEP} :");
+                listener.Send(new byte[]{1},1, groupEP);
+                Console.WriteLine($" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            listener.Close();
+        }
+    }
+
+    public static void Main()
+    {
+        StartListener();
+    }
+}
